cd8b34505733d245b8c11dbd1e43f84b20850cc8
Mariia2811 <mary.stupka11@gmail.com>
2023-04-23 19:35:47 +0300

    create task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-console.log('1')
+const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+
+  let intervalId;
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+
+  function startChangingBackgroundColor() {
+    intervalId = setInterval(changeBackgroundColor, 1000);
+    startBtn.disabled = true;
+  }
+
+  function stopChangingBackgroundColor() {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+  }
+
+  startBtn.addEventListener('click', startChangingBackgroundColor);
+  stopBtn.addEventListener('click', stopChangingBackgroundColor);
+  
