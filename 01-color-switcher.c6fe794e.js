f91b80c9fd9d5c71b1a4248f2fcda7beaabcdc45
Mariia2811 <116021359+Mariia2811@users.noreply.github.com>
2023-04-23 16:36:17 +0000

    Deploying to gh-pages from @ Mariia2811/goit-js-hw-09@cd8b34505733d245b8c11dbd1e43f84b20850cc8 🚀

---
+++ 01-color-switcher.c6fe794e.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){t=setInterval(a,1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.c6fe794e.js.map
--- 01-color-switcher.efe11fa6.js
+++
@@ -1,2 +0,0 @@
-console.log("1");
-//# sourceMappingURL=01-color-switcher.efe11fa6.js.map
